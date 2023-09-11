12f6a8d9220c2743e8e6fa7c587533dd94c4eb67
Miguel Mejia <miguelingil89@gmail.com>
2023-09-11 17:13:43 -0500

    se agrega bucle infinito

--- inicio.py
+++ inicio.py
@@ -1,2 +1,22 @@
-nombre = input ("nombre de usuario:")
+nombre = input ("escribe tu nombre:")
 print ("bienvenido", nombre)
+
+
+
+import readchar
+from readchar import readkey, key
+
+while True:
+  
+ try:
+  key = readchar.readkey()
+  print(f"tecla presionada:{key}")
+  if key == 'w':
+   print("la tecla ↑ fue presionada")
+   break
+ except KeyboardInterrupt:
+  break
+ print("programa terminado")
+
+  
+   
