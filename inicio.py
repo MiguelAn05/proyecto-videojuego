0eed964497a24f59dc22a6b41d0ee8f57330fee1
Miguel Mejia <miguelingil89@gmail.com>
2023-09-13 16:25:53 -0500

    agrega programa que cada tecla n presionada aumenta un numero

--- inicio.py
+++ inicio.py
@@ -12,11 +12,32 @@ while True:
   key = readchar.readkey()
   print(f"tecla presionada:{key}")
   if key == 'w':
-   print("la tecla ↑ fue presionada")
+   print("la tecla w fue presionada")
    break
  except KeyboardInterrupt:
   break
- print("programa terminado")
 
-  
+
+
+
+import os
+import readchar
+n=0
+print("Presiona la tecla 'n' para aumentar el número")
+while n < 50:
+  try:
+    key=readchar.readkey()
+    if key == 'n':
+     n+=1
+     os.system('cls' if os.name=='nt' else 'clear')
+     print(f"numero:{n}")
+    else:
+     break
+  except KeyboardInterrupt:
+   break
+   
+
+
+
+ 
    
